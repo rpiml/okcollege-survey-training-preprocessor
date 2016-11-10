c7793868dccb4efaf4a06ef5959fd9a43a95b937
zlalvani <zlalvani@gmail.com>
2016-11-09 19:39:37 -0500

    survey data is now set on redis

--- helpers.py
+++ helpers.py
@@ -2,62 +2,42 @@ import csv
 import json
 import io
 
-def _compute_values(question, type_dict):
-    if question['type'] in ['slider', 'choice', 'multi-choice-dropdown']:
-        type_dict[question['id']] = question['type']
-        writer.writerow([question['id'], question['type']])
-    elif question['type'] == 'multi-choice':
-        for q in question['answers']:
-            type_dict[question['id'] + ':' + q] = 'boolean'
-            writer.writerow([question['id'] + ':' + q, 'boolean'])
-    else:
-        raise Exception("Malformed question type value in survey_response")
-        # if question['type'] == 'slider':
-        #     return [question['answer']]
-        # elif question['type'] == 'multi-choice':
-        #     answers = dict(zip(question['answers'], range(len(question['answers']))))
-        #     answer = question['answer']
-        #
-        #     value = 0
-        #
-        #     for ans in answer:
-        #         i = answers[ans]
-        #         value &= 2 ** i
-        #
-        #     return value
-        #
-        # elif question['type'] == 'choice':
-        #     pass
-        # elif question['type'] == 'multi-choice-dropdown':
-        #     pass
-
 def process_survey_result(result, type_dict):
     response_table = io.StringIO()
     writer = csv.writer(response_table, delimiter=',')
     try:
         for row in result:
             response_vector = []
-            for page in row[0]['survey']['pages']:
+            for page in row[1]['survey']['pages']:
                 for question in page['questions']:
-                    if question['type'] == 'slider':#['slider', 'choice', 'multi-choice-dropdown']:
+                    if 'answer' not in question:
+                        response_vector.append((question['id'], None))
+                        continue
 
-                        response_vector.append(float(question['answer']))
-                        # response_vector.append(float())
-                        # type_dict[question['id']] = question['type']
-                        # writer.writerow([question['id'], question['type']])
+                    if question['type'] == 'slider':#['slider', 'choice', 'multi-choice-dropdown']:
+                        assert(question['id'] in type_dict)
+                        response_vector.append((question['id'], float(question['answer'])))
 
                     elif question['type'] == 'choice' or question['type'] == 'multi-choice-dropdown':
-                        pass
+                        assert(question['id'] in type_dict)
+                        i = question['answers'].index(question['answer'])
+                        response_vector.append((question['id'], i))
+
                     elif question['type'] == 'multi-choice':
+                        answer = set(question['answer'])
                         for q in question['answers']:
                             name = question['id'] + ':' + q
-                            # type_dict[question['id'] + ':' + q] = 'boolean'
-                            # writer.writerow([question['id'] + ':' + q, 'boolean'])
+                            assert(name in type_dict)
+                            if q in answer:
+                                response_vector.append((name, 1.))
+                            else:
+                                response_vector.append((name, 0.))
                     else:
                         raise Exception('Malformed question type value in form')
-                    # print(type(question))
-                    # values = _compute_values(question, type_dict)
-            writer.writerow([v for v in sorted(response_vector, key=lambda x: x[0])])
+
+            writer.writerow([str(row[0])] + [v[1] for v in sorted(response_vector, key=lambda x: x[0])])
+
+        return response_table
 
     except LookupError:
         raise Exception('Malformed JSON object in survey_response')
@@ -71,30 +51,21 @@ def construct_type_table(form_loc='assets/form.json'):
         try:
             for page in form['pages']:
                 for question in page['questions']:
-                    # print(type(question))
                     if question['type'] == 'slider':
                         type_dict[question['id']] = ('numerical', None)
-                        writer.writerow([question['id'], 'numerical', ''])
                     elif question['type'] == 'choice' or question['type'] == 'multi-choice-dropdown':
                         type_dict[question['id']] = ('categorical', str(len(question['answers'])))
-                        writer.writerow([question['id'] + ':' + q, 'categorical', str(len(question['answers'])]))
                     elif question['type'] == 'multi-choice':
                         for q in question['answers']:
                             type_dict[question['id'] + ':' + q] = ('categorical', 2)
-                            writer.writerow([question['id'] + ':' + q, 'categorical', str(2)])
                     else:
                         raise Exception('Malformed question type value in form')
-                        # answer = set(question['answer'])
-                        # for q in question['answers']:
-                        #     if q in answer:
-                        #         questions[question['id'] + ':' + q] = 1.
-                        #         writer.writerow()
-                        #     else:
-                        #         questions[question['id'] + ':' + q] = 0.
 
-
-
+            for k in sorted(type_dict.keys()):
+                feature_type, num = type_dict[k]
+                writer.writerow([k, feature_type, num])
 
             return type_dict, question_table
+
         except LookupError:
             raise Exception('Malformed JSON in form')
--- preprocessor.py
+++ preprocessor.py
@@ -1,14 +1,19 @@
 import pg8000
 import json
+import redis
 import helpers
 
 if __name__ == '__main__':
     conn = pg8000.connect(database='okcollege_dev', user='postgres', password='')
     cursor = conn.cursor()
-    cursor.execute('SELECT content FROM survey_response')
+    cursor.execute('SELECT uuid, content FROM survey_response')
     result = cursor.fetchall()
     cursor.close()
     conn.close()
 
-    type_dict, question_table = helpers.construct_type_table('../assets/form.json')
-    helpers.process_survey_result(result)
+    type_dict, question_table = helpers.construct_type_table('assets/form.json')
+    result_table = helpers.process_survey_result(result, type_dict)
+
+    r = redis.StrictRedis(host='localhost')
+    r.set('learning:survey_training.csv', result_table.getvalue())
+    r.set('learning:survey_features.csv', question_table.getvalue())
