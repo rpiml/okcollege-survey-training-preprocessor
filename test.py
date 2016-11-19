6f68f319ae06af827a21486ae6e37bb6a4a5f76a
zlalvani <zlalvani@gmail.com>
2016-11-19 05:45:30 -0500

    implemented unittest

--- test.py
+++ test.py
@@ -2,49 +2,56 @@ import pg8000
 import json
 import redis
 import helpers
+import unittest
 
-if __name__ == '__main__':
-    with open('test_assets/example_survey.csv') as example_survey, \
-            open('test_assets/example_features.csv') as example_features, \
-            open('test_assets/example_uuid.txt') as example_uuid, \
-            open('test_assets/example_response.json') as example_response:
+class TestHelperFunctions(unittest.TestCase):
+    def setUp(self):
+        self.example_survey = open('test_assets/example_survey.csv')
+        self.example_features = open('test_assets/example_features.csv')
+        self.example_uuid = open('test_assets/example_uuid.txt')
+        self.example_response = open('test_assets/example_response.json')
 
         helpers.query_db('CREATE TABLE survey_response (uuid uuid, content json)', database='postgres', response=False, autocommit=True)
 
         ins_query = "INSERT INTO survey_response VALUES ('%s', '%s')" % \
-                    ("782786f0-a6fa-11e6-8a53-795ffb01e23b", json.dumps(json.load(example_response)))
+                    ("782786f0-a6fa-11e6-8a53-795ffb01e23b", json.dumps(json.load(self.example_response)))
 
         helpers.query_db(ins_query, database='postgres', response=False, autocommit=True)
 
         result = helpers.query_db('SELECT uuid, content FROM survey_response', database='postgres')
 
-        type_dict, question_table = helpers.construct_type_table('assets/form.json')
+        type_dict, question_table = helpers.construct_type_table('test_assets/form.json')
         result_table = helpers.process_survey_result(result, type_dict)
 
-        r = redis.StrictRedis(host='localhost')
-        r.set('learning:survey_training.csv', result_table.getvalue())
-        r.set('learning:survey_features.csv', question_table.getvalue())
+        self.r = redis.StrictRedis(host='localhost')
+        self.r.set('learning:survey_training.csv', result_table.getvalue())
+        self.r.set('learning:survey_features.csv', question_table.getvalue())
 
-        output_survey = r.get('learning:survey_training.csv')
-        output_features = r.get('learning:survey_features.csv')
+    def tearDown(self):
+        self.example_survey.close()
+        self.example_features.close()
+        self.example_uuid.close()
+        self.example_response.close()
 
-        example_survey_text = example_survey.read().rstrip()
+        helpers.query_db('DROP TABLE survey_response', database='postgres', response=False, autocommit=True)
+
+    def test_survey_csv(self):
+        output_survey = self.r.get('learning:survey_training.csv')
         output_survey_text = output_survey.decode('utf-8').rstrip()
+        example_survey_text = self.example_survey.read().rstrip()
 
-        print('Following 2 lines should be identical:')
-        print(example_survey_text)
-        print('-' * 30)
-        print(output_survey_text)
-        assert(example_survey_text == output_survey_text)
+        self.assertEqual(output_survey_text, example_survey_text)
 
-        example_features_list = example_features.read().split('\n')
+    def test_features_csv(self):
+        output_features = self.r.get('learning:survey_features.csv')
         output_features_list = output_features.decode('utf-8').split('\r\n')
+        example_features_list = self.example_features.read().split('\n')
 
-        print('%d and %d should be identical' % (len(example_features_list), len(output_features_list)))
-        print(len(example_features_list), len(output_features_list))
-        assert(len(example_features_list) == len(output_features_list))
+        self.assertEqual(len(example_features_list), len(output_features_list))
 
-        print('\nFollowing 2 tables should be identical:')
         for i, line in enumerate(example_features_list):
-            print('%s <-> %s' % (line.strip(), output_features_list[i].strip()))
-            assert(line.strip() == output_features_list[i].strip())
+            self.assertEqual(line.strip(), output_features_list[i].strip())
+
+
+if __name__ == '__main__':
+    unittest.main()
